4b6ef10ee9175b4d14ca5648e8b91f3934b09fef
Natalia Luiza <natalialuizas@live.com>
2020-05-24 16:29:39 -0300

    first commit

---
+++ javascript/produtos.js
@@ -0,0 +1,81 @@
+const produtos = [
+   { 
+      nome: 'Pão integral 6 grãos 450g',
+      descricao: 'contém ovos',
+      valor: 'R$10,00' ,
+      imgUrl: './img/pao-integral-03.jpg'
+   
+   }, 
+   { 
+      nome: 'Pão Vegano 450g',
+      descricao: 'contém glúten',
+      valor: 'R$10,00',
+      imgUrl: './img/Pão-Caseiro-Fofinho.png'
+
+   }, 
+   { 
+      nome: 'Bolo de Banana c/ aveia 450g',
+      descricao: 'sem glúten, sem adição de açúcar, contém ovos',
+      valor: 'R$15,00',
+      imgUrl: './img/bolo-de-banana.jpg'
+
+   },
+   { 
+      nome: 'Bolo de Cacau 450g',
+      descricao: 'Vegano, contén glúten',
+      valor: 'R$15,00',
+      imgUrl: './img/bolo-de-cacau.jpg'
+
+   },  
+   { 
+      nome: 'Bolo de Milho 500g',
+      descricao: ' Sem glúten, sem leite, contém ovos ',
+      valor: 'R$20,00',
+      imgUrl: './img/bolo-de-milho.png'
+   },
+
+  
+{ 
+   nome: 'Mousse de Maracujá 120g',
+   descricao: 'contém leite',
+   valor: 'R$7,00',
+   imgUrl: './img/mousse-maracuja.png'
+},
+
+{ 
+   nome: 'Geleia de Chia Sabor Uva 230 ml',
+   descricao: 'Sem Glúten, produzido com açúcar orgânico ou xilitol (adoçante natural)',
+   valor: 'R$15,00',
+   imgUrl: './img/geleia-de-chia2.jpg'
+},
+
+{
+nome: 'Creme de Morango 100g',
+   descricao: 'Feita a base de inhame e morango, contém açúcar ',
+   valor: 'R$6,00',
+   imgUrl: './img/Iogurte_Vegano_Morango.jpg'
+},
+
+{
+   nome: 'Cestas Com Caneca Personalizada ',
+      descricao: 'contém: Pão Integral ou Vegano, Geleia de Chia, Bolo de 150g, Cupcakes de bolo de banana ',
+      valor: ' A partir R$90,00',
+      imgUrl: './img/cesta-de-comidas.jpg'
+   },
+
+   {
+      nome: 'Cestas Sem Caneca Personalizada ',
+         descricao: 'contém: Pão Integral ou Vegano, Geleia de Chia, Bolo de 150g, Cupcakes de bolo de banana ',
+         valor: ' A partir R$70,00',
+         imgUrl: './img/cesta-com-vinho.jpg'
+      },
+
+      {
+         nome: 'Sacolas Personalizada',
+            descricao: 'contém: Pão Integral ou Vegano, Geleia de Chia, Bolo de cacau 150g ou Cupcakes de bolo de banana ',
+            valor: ' A partir R$60,00',
+            imgUrl: './img/sacola-produtos-presenteavel.jpg'
+         }
+   ]
+
+
---
+++ javascript/scripts.js
@@ -0,0 +1,16 @@
+const sectionCards = document.querySelector(".cards");
+
+const card = document.querySelector("div.card");
+
+produtos.map(produto => {
+  const cardClone = card.cloneNode(true);
+  cardClone.querySelector("img").src = produto.imgUrl;
+  cardClone.querySelector(".title").innerHTML = produto.nome;
+  cardClone.querySelector(".info > p.text__price").innerHTML =
+    produto.valor;
+  sectionCards.appendChild(cardClone);
+});
+
+card.remove();
+
+
